1a7612127f087c8f6703d7492a41ca320fd5d433
jordanwentland <jordanwentland@gmail.com>
2022-11-26 00:54:22 -0500

    code cleanup

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 app.use( express.static( __dirname + '/public' ));
 
-// sendFile will go here
 app.get('/', function(req, res) {
     res.sendFile( path.join( __dirname, 'public', 'index.html' ));
 });
